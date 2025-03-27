ddbe97e253dc98c0975c214e25d1d00ece10e588
Georgy <georgy.kurmashev@gmail.com>
2025-03-27 17:31:10 +0300

    Добавлена простая регистрация и авторизация

--- backend/main.js
+++ backend/main.js
@@ -1,30 +1,14 @@
 import express from "express";
-import path from "path";
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
+import { router } from "./authRouter.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-// API маршрут для проверки пароля
-app.post('/api/check-password', (req, res) => {
-    // Логика проверки пароля
-    if (req.body.password) {
-        res.status(200).send({ success: true });
-    }
-    else {
-        res.status(401).send({ success: false });
-    }
-});
-// Для всех остальных маршрутов возвращаем
-app.get('*', (req, res) => {
-    res.send('Hello World!');
-});
-// Обработка маршрута '/'
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+app.use("/auth", router);
+
 const start = () => {
     try {
+        // TODO: Конектимся к базе..
         app.listen(port, () => console.log(`Server started on port ${port}`));
     }
     catch (e) {
